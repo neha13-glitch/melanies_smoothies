5a9342d57a560e2db0fb306cd3cb71c0c323fc22
neha13-glitch <nehasjadhav@eaton.com>
2025-07-01 18:10:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,9 @@ name_on_order = st.text_input("Name on smoothie :")
 st.write("The name on your smoothie will be :", name_on_order)
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('search_on'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 ingredients_list=st.multiselect('choose upto 5 ingredients:',my_dataframe,max_selections=5)
 
 if ingredients_list:
